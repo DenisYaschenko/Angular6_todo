c5a2986b12bb6b7a94b260fac5e8d10a3decfebc
Denis <denis1999yaschenko@gmail.com>
2018-07-26 16:22:21 +0300

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  model = new Model();
+
+  getTodoItems() {
+    return this.model.items;
+  }
+
+  AddItem(newItem) {
+    if (newItem === '') {
+      alert('You shouldn\'t do it');
+    }  else {
+      this.model.items.push(new ToDoItems(newItem, false));
+    }
+  }
+}
+
+export class Model {
+  items;
+
+  constructor() {
+    this.items = [new ToDoItems('Buy something', false),
+    new ToDoItems('Buy flower', false),
+    new ToDoItems('Buy beer', false),
+    new ToDoItems('Buy whore', false)];
+  }
+}
+
+export class ToDoItems {
+  descr;
+  done;
+
+  constructor (descr, done) {
+    this.descr = descr;
+    this.done = done;
+  }
 }
